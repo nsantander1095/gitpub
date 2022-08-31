46cb078e8bf1b26c08b966dddbf9100ae26e5b63
unknown <jason.rettinger@gmail.com>
2022-08-31 13:25:44 -0400

    added for loop/append children

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,29 @@ var toJSON = function(response){
     return response.json();
 };
 var displayResults = function(data){
-    
+    console.log(data)
+    var resultsEl = document.getElementById("resultsBin");
+    resultsEl.innerHTML = null;
+    for (var i=0; i<data.length; i++) {
+        var resultsItemEl = document.createElement("card");
+        var breweryNameEl = document.createElement("h3");
+        var breweryAddressEl = document.createElement("p");    
+        var breweryPhoneEl = document.createElement("p");    
+        var breweryURLEl = document.createElement("p");    
+        breweryNameEl.textContent = data[i].name;
+        breweryAddressEl.textContent = "Address: " + data[i].street +" " +  data[i].state;
+        breweryPhoneEl.textContent = "Phone Number: " + data[i].phone;
+        breweryURLEl.textContent = "Web Site: " + data[i].website_url;
+        resultsEl.appendChild(resultsItemEl);
+        resultsItemEl.appendChild(breweryNameEl);
+        resultsItemEl.appendChild(breweryAddressEl);
+        resultsItemEl.appendChild(breweryPhoneEl);
+        resultsItemEl.appendChild(breweryURLEl);
+    }  
 }
 var brewURL = `https://api.openbrewerydb.org/breweries?by_city=${city}`;
 fetch(brewURL)
     .then(toJSON)
     .then(function(data){
-        console.log(data)
         displayResults(data)
     });
